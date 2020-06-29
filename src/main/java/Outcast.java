38a823385b710dde33d12c0fa339b48ebdb84a72
viktor <viktor.zikratyi@gmail.com>
2020-06-29 21:22:23 +0300

    Implemented Outcast

---
+++ src/main/java/Outcast.java
@@ -0,0 +1,47 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Outcast {
+    private final WordNet wordNet;
+
+    public Outcast(WordNet wordnet) {
+        if (wordnet == null) {
+            throw new IllegalArgumentException();
+        }
+        this.wordNet = wordnet;
+    }
+
+    public String outcast(String[] nouns) {
+        if (nouns == null) {
+            throw new IllegalArgumentException();
+        }
+        String currentOutcast = null;
+        int currentMaxDistance = Integer.MIN_VALUE;
+        for (String noun : nouns) {
+            if (noun == null) {
+                throw new IllegalArgumentException();
+            }
+            int currentDistance = 0;
+            for (String otherNoun : nouns) {
+                currentDistance += wordNet.distance(noun, otherNoun);
+            }
+            if (currentDistance > currentMaxDistance){
+                currentMaxDistance = currentDistance;
+                currentOutcast = noun;
+            }
+        }
+        return currentOutcast;
+    }
+
+
+
+    public static void main(String[] args) {
+        WordNet wordnet = new WordNet(args[0], args[1]);
+        Outcast outcast = new Outcast(wordnet);
+        for (int t = 2; t < args.length; t++) {
+            In in = new In(args[t]);
+            String[] nouns = in.readAllStrings();
+            StdOut.println(args[t] + ": " + outcast.outcast(nouns));
+        }
+    }
+}
--- src/main/java/WordNet.java
+++ src/main/java/WordNet.java
@@ -22,7 +22,7 @@ public class WordNet {
 
 
     public WordNet(String synsets, String hypernyms) {
-        if (synsets == null || hypernyms == null){
+        if (synsets == null || hypernyms == null) {
             throw new IllegalArgumentException();
         }
         fillSynsets(synsets);
@@ -71,6 +71,7 @@ public class WordNet {
                 String hypernymsLine = in.readLine();
                 int index = extractIndex(hypernymsLine);
                 String hypernym = extractHypernym(hypernymsLine);
+                if (hypernym.isEmpty()) continue;
                 for (String relationshipIndexStr : parseHypernyms(hypernym)) {
                     synsetsDigraph.addEdge(index, Integer.parseInt(relationshipIndexStr));
                 }
@@ -80,28 +81,31 @@ public class WordNet {
         }
     }
 
-    private Integer extractIndex(String synsetStr){
+    private Integer extractIndex(String synsetStr) {
         int firstComa = synsetStr.indexOf(",");
-        String indexStr = synsetStr.substring(0, firstComa);
+        String indexStr = firstComa != -1 ?
+                synsetStr.substring(0, firstComa)
+                : synsetStr;
         return Integer.parseInt(indexStr);
     }
 
-    private String extractSynset(String synsetStr){
+    private String extractSynset(String synsetStr) {
         int firstComa = synsetStr.indexOf(",");
-        int secondComa = synsetStr.indexOf(",", firstComa);
-        return synsetStr.substring(firstComa+1, secondComa);
+        int secondComa = synsetStr.indexOf(",", firstComa + 1);
+        return synsetStr.substring(firstComa + 1, secondComa);
     }
 
-    private String extractHypernym(String hypernymsStr){
+    private String extractHypernym(String hypernymsStr) {
         int firstComa = hypernymsStr.indexOf(",");
-        return hypernymsStr.substring(firstComa+1);
+        return firstComa != -1 ?
+                hypernymsStr.substring(firstComa + 1) : "";
     }
 
-    private String[] parseSynsets(String synset){
+    private String[] parseSynsets(String synset) {
         return synset.split(" ");
     }
 
-    private String[] parseHypernyms(String hypernym){
+    private String[] parseHypernyms(String hypernym) {
         return hypernym.split(",");
     }
 
@@ -112,17 +116,17 @@ public class WordNet {
 
     // is the word a WordNet noun?
     public boolean isNoun(String word) {
-        if (word == null){
+        if (word == null) {
             throw new IllegalArgumentException();
         }
         return nounIndexes.containsKey(word);
     }
 
-    public int distance(String nounA, String nounB){
-        if (nounA == null || nounB == null){
+    public int distance(String nounA, String nounB) {
+        if (nounA == null || nounB == null) {
             throw new IllegalArgumentException();
         }
-        if (!isNoun(nounA) || !isNoun(nounB)){
+        if (!isNoun(nounA) || !isNoun(nounB)) {
             throw new IllegalArgumentException();
         }
         if (nounA.equals(nounB)) return 0;
@@ -132,10 +136,10 @@ public class WordNet {
     // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
     // in a shortest ancestral path
     public String sap(String nounA, String nounB) {
-        if (nounA == null || nounB == null){
+        if (nounA == null || nounB == null) {
             throw new IllegalArgumentException();
         }
-        if (!isNoun(nounA) || !isNoun(nounB)){
+        if (!isNoun(nounA) || !isNoun(nounB)) {
             throw new IllegalArgumentException();
         }
         if (nounA.equals(nounB)) return nounA;
