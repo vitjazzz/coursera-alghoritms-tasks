a719edfc341f5b6d7227cdccfff35dcc31d7eb69
vzikratyi <vzikratyi@thingsboard.io>
2021-01-17 13:42:54 +0200

    Moved boggle solver to separate package

--- src/main/java/boggle/BoggleBoard.java
+++ src/main/java/boggle/BoggleBoard.java
@@ -1,6 +1,6 @@
-/******************************************************************************
- *  Compilation:  javac BoggleBoard.java
- *  Execution:    java  BoggleBoard
+package boggle; /******************************************************************************
+ *  Compilation:  javac boggle.BoggleBoard.java
+ *  Execution:    java  boggle.BoggleBoard
  *  Dependencies: StdRandom.java In.java StdOut.java
  *
  *  A data type for Boggle boards.
@@ -192,7 +192,7 @@ public class BoggleBoard {
     }
 
     /**
-     * Unit tests the BoggleBoard data type.
+     * Unit tests the boggle.BoggleBoard data type.
      */
     public static void main(String[] args) {
 
--- src/main/java/boggle/BoggleSolver.java
+++ src/main/java/boggle/BoggleSolver.java
@@ -1,3 +1,5 @@
+package boggle;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
